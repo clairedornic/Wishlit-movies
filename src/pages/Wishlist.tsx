2ed0c2b8fd7e6bf581a59768cd193d7480389e30
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-09-02 15:16:49 +0200

    feat: add movies view and possibility to search a movie by title

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 import '../styles/wishlist/wishlist.scss';
 import Header from '../components/wishlist/Header';
 import MoviesView from '../components/wishlist/MoviesView';
+import moviesData from '../assets/datas/data.json'
+import { Movie } from '../types/Movie';
 
 function App() {
-  const [selectedCategories, setSelectedCategories] = useState<string>('All');
-  const [selectedView, setSelectedView] = useState<string>('grid');
+  const initialMovies = moviesData.movie as Movie[];
+  const [movies, setMovies] = useState(initialMovies);
 
-  const handleCategoryChange = (category: string) => {
-    setSelectedCategories(category);
+  const performSearch = (query: string) => {
+    if (query === '') {
+      setMovies(initialMovies);
+    } else {
+      const filteredMovies = initialMovies.filter((movie) =>
+        movie.title.toLowerCase().includes(query.toLowerCase())
+      );
+      setMovies(filteredMovies);
+    }
   };
-
-  const handleViewChange = (view: string) => {
-    setSelectedView(view);
-  };
-
   return (
     <div className="wishlist">
-      <Header onCategoryChange={handleCategoryChange} onViewChange={handleViewChange}/>
-      <MoviesView></MoviesView>
+      <Header onSearch={performSearch} />
+      <MoviesView movies={movies}></MoviesView>
     </div>
   );
 }
