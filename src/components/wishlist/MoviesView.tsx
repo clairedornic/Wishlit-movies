b649ab1a18c23c260d493b9235fc61ffa4a7683e
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-09-02 15:17:36 +0200

    feat: add movies view

--- src/components/wishlist/MoviesView.tsx
+++ src/components/wishlist/MoviesView.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import HeaderMovies from './HeaderMovies'
+import GridView from './GridView';
+import { Movie } from '../../types/Movie';
 
-const MoviesView: React.FC = () => {
+interface MoviesViewProps {
+  movies: Movie[]
+}
+
+const MoviesView: React.FC<MoviesViewProps> = ({movies}) => {
 
   return (
-    <main>
-        {/* HeaderMovies */}
-        {/* GridView */}
-        {/* ListView */}
+    <main className='movies'>
+        <HeaderMovies countMovies={movies.length}></HeaderMovies>
+        <GridView movies={movies}></GridView>
     </main>
   )
 }
