b6ee53dfd1a85234d4b8d5997a4757f66a5afbb6
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-09-02 15:18:44 +0200

    refactor: remove useless event for the test and change a name for styleType

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
 interface ButtonProps {
-    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
     label?: string;
     icon?: React.ReactNode;
-    styleType?: 'primary' | 'toggle' | 'delete' | 'filter' | 'tag';
+    styleType?: 'primary' | 'toggle' | 'delete' | 'filter' | 'category';
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, icon, label, styleType = 'primary' }) => {
+const Button: React.FC<ButtonProps> = ({ icon, label, styleType = 'primary' }) => {
 
     return (
         <button
-            onClick={onClick}
             className={`button ${styleType}`}
         >
             {icon && <span className="button__icon">{icon}</span>}
