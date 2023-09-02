17ba9febc4c65d0d05cef1b3fc64b5824bd1f7b4
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-09-02 15:14:13 +0200

    refactor: remove useless event for the test

--- src/components/wishlist/ViewChoice.tsx
+++ src/components/wishlist/ViewChoice.tsx
@@ -4,15 +4,14 @@ import iconGrid from '../../assets/icons/icon-grid.svg';
 import iconList from '../../assets/icons/icon-list.svg';
 
 interface ViewProps {
-  onViewChange: (c: string) => void;
   className: string
 }
 
-const ViewChoice: React.FC<ViewProps> = ({onViewChange, className}) => {
+const ViewChoice: React.FC<ViewProps> = ({className}) => {
   return (
     <div className={className}>
-        <Button onClick={() => onViewChange("grid")} label="" icon={<img src={iconGrid} alt="A grid" />} styleType="toggle" />
-        <Button onClick={() => onViewChange("list")} label="" icon={<img src={iconList} alt="A list" />} styleType="toggle" />
+        <Button label="" icon={<img src={iconGrid} alt="A grid" />} styleType="toggle" />
+        <Button label="" icon={<img src={iconList} alt="A list" />} styleType="toggle" />
     </div>
   )
 }
